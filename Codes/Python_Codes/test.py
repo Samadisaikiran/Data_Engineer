96d04723b18e60b66b9e63cfd150737d30c0d00a
saikiran <113518611+Samadisaikiran@users.noreply.github.com>
2026-08-19 00:41:32 +0530

    commit

---
+++ Codes/Python_Codes/test.py
@@ -0,0 +1,18 @@
+print("Python is working!")
+print("Ready for email automation!")
+
+import imaplib
+print("imaplib imported successfully!")
+
+import email
+print("email imported successfully!")
+
+import subprocess
+print("subprocess imported successfully!")
+
+print("All modules ready!")
+
+#finding python in local/windows/command prompt
+
+#dir C:\Python* /s /b 2>nul
+#dir /s /b python.exe 2>nul
