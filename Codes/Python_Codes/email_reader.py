98a1b92ce2548204de27f7b2c768572a8c6877a9
saikiran <113518611+Samadisaikiran@users.noreply.github.com>
2026-08-19 21:17:29 +0530

    commit

--- Codes/Python_Codes/email_reader.py
+++ Codes/Python_Codes/email_reader.py
@@ -20,8 +20,8 @@ from email.header import decode_header
 #------------------------------------------
 # CONFIGURATION
 #------------------------------------------
-EMAIL_ADDRESS   = "your gmail@gmail.com"
-APP_PASSWORD    = "gmail 16 charcters password"
+EMAIL_ADDRESS   = "yourgmail@gmail.com"
+APP_PASSWORD    = "gmail16charcters password"
 IMAP_SERVER     = "imap.gmail.com"
 TRIGGER_SUBJECT = "ACAPS Data Load Complete"
 BASH_PATH       = "C:/Program Files/Git/bin/bash.exe"
